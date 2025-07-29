1525ac2147b52cd4aa3997e67b8b5b4e6c85d64e
Kaizen-pixel01 <kvasudevan@torontomu.ca>
2025-07-29 00:47:47 -0400

    Update generate_bboxes_from_masks.py

--- MedSAM_zero_shot/generate_bboxes_from_masks.py
+++ MedSAM_zero_shot/generate_bboxes_from_masks.py
@@ -9,11 +9,11 @@ import os
 import cv2
 import json
 from tqdm import tqdm
-#specifying where the ground truth (gt) masks are 
+# specifying where the masks masks are (which is what the boxes are based on)
 mask_dir = "/content/drive/MyDrive/MedSAM/data/ctrus/masks"
 output_json = "/content/drive/MyDrive/MedSAM/data/ctrus/bboxes.json"
 
-annotations = [] #initialize
+annotations = [] #initialize to store the annothations
 
 for filename in tqdm(os.listdir(mask_dir)): #looping thru all masks
     if not filename.endswith('.jpg'):
@@ -23,12 +23,12 @@ for filename in tqdm(os.listdir(mask_dir)): #looping thru all masks
     mask = cv2.imread(mask_path, 0) #grayscale 
 
     if mask is None:
-        print(f"Skipped unreadable: {filename}") #in case there were issues when downloading the images
+        print(f"Skipped: {filename}") #in case there were issues when downloading the images
         continue
 
     _, binary_mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY) #needed to convert a grayscale image to a binary mask for the boxes (255 as white)
     if not (binary_mask > 0).any():
-        print(f"No foreground in: {filename}") #to avoid image pairs with no masks
+        print(f"No mask in: {filename}") #to avoid image pairs with no masks
         continue
 
     x, y, w, h = cv2.boundingRect((binary_mask > 0).astype('uint8'))
@@ -37,7 +37,7 @@ for filename in tqdm(os.listdir(mask_dir)): #looping thru all masks
         "bbox": [x, y, x + w, y + h] #specifying format for boxes (led to issues in intial runs)
     })
 
-#making sure output is saved in proper folder
+# making sure output is saved in proper folder
 with open(output_json, "w") as f: 
     json.dump(annotations, f, indent=2)
 
