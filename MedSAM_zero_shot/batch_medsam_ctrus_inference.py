a3329e43946b19f05a45b1f1014126cdafc340fb
Kaizen-pixel01 <kvasudevan@torontomu.ca>
2025-07-29 01:04:50 -0400

    Update batch_medsam_ctrus_inference.py

--- MedSAM_zero_shot/batch_medsam_ctrus_inference.py
+++ MedSAM_zero_shot/batch_medsam_ctrus_inference.py
@@ -20,21 +20,24 @@ bbox_json = "/content/drive/MyDrive/MedSAM/data/ctrus/bboxes.json"
 checkpoint = "/content/drive/MyDrive/MedSAM/work_dir/MedSAM/medsam_vit_b.pth"
 output_dir = "/content/drive/MyDrive/MedSAM/data/ctrus/predicted_masks"
 
-os.makedirs(output_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True) #helps to make sure that the needed paths were made and named properly (also helps for when you need to debug errors)
 
-# Load bounding boxes 
+# Load bounding boxes - different format from the CLI used in medsam inference file since it kept running errors with C-trus images
 with open(bbox_json, "r") as f:
     bboxes = json.load(f)
 bbox_dict = {entry["image"]: entry["bbox"] for entry in bboxes}
 
-# Load MedSAM Model 
+# Load MedSAM Model - very similar methodology to inference file with single image
 device = "cuda" if torch.cuda.is_available() else "cpu"
 sam = sam_model_registry["vit_b"](checkpoint=None) #using MedSAM pretrained checkpoint as provided in gitub repo
 state_dict = torch.load(checkpoint, map_location=torch.device("cpu")) 
 sam.load_state_dict(state_dict)  #loads all the necessary weights as well since that wont be change d in zero shot
 sam.to(device)
+
 predictor = SamPredictor(sam) #needed for prompt inference (i.e image loading, prompt processing, etc)
- 
+'''
+had troubles adapting MedSAm repo version since i didn't use CLI for the prompt boxes like with the example so I had to refer to SAM repo to use alternative function
+'''
 # Limit to 10 valid images for testing if the model works (can remove after making sure it works - so just getting rif of the limit)
 processed = 0
 skipped = 0
@@ -47,12 +50,13 @@ for filename in list(bbox_dict.keys()):
         break
 
     image_path = os.path.join(image_dir, filename) #loading image
+    
     #for debugging since the first download got interupted and it saved empty files
     if not os.path.exists(image_path):
         print(f" Skipping (image file not found): {filename}")
         skipped += 1
         continue
-
+    #same thought process - avoid any kind of daulty files
     image = cv2.imread(image_path)
     if image is None:
         print(f" Skipping (cannot read image): {filename}")
@@ -73,6 +77,7 @@ for filename in list(bbox_dict.keys()):
             torch.tensor(input_box, dtype=torch.float32, device=device),
             image.shape[:2] #transformations needed to match others
         )
+        
         #zero shot so no prompt points
         masks, scores, _ = predictor.predict_torch(
             point_coords=None,
@@ -80,6 +85,7 @@ for filename in list(bbox_dict.keys()):
             boxes=transformed_box,
             multimask_output=False #only need one mask
         )
+        
         #saving the prediction
         pred_mask = (masks[0][0].cpu().numpy() * 255).astype(np.uint8)
         out_path = os.path.join(output_dir, filename.replace(".jpg", "_mask.png"))
@@ -93,7 +99,7 @@ for filename in list(bbox_dict.keys()):
         skipped += 1
 
 # final Summary 
-print(f"\n DONE!")
+print(f"\n DONE FINALLY!")
 print(f"Total processed: {processed}")
 print(f"Total skipped: {skipped}")
 
