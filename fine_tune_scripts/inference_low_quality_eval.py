3ba5ea7c0355e2301f0e392a4683a483d3cdc1a1
Kaizen-pixel01 <kvasudevan@torontomu.ca>
2025-08-02 15:26:54 -0400

    Update inference_low_quality_eval.py

--- fine_tune_scripts/inference_low_quality_eval.py
+++ fine_tune_scripts/inference_low_quality_eval.py
@@ -87,9 +87,10 @@ class MedSAM(nn.Module): #ensuring pretrained componets are being used - freezin
 #  Metrics 
 def compute_metrics(preds, targets): #same format as training script - needed for comparisons amongst models
     smooth = 1e-8  #avoid zero calculations
+    #flattening both masks so that the calculations for the metrics can be made easily (without errors poping up for shaping (since it wont work if the dimenstions are different for the two masks)
     preds = preds.view(preds.shape[0], -1)
     targets = targets.view(targets.shape[0], -1)
-
+    #makes it easier to count which parts (pixels in the case of the images used) match with the ground truth (gts)
     intersection = (preds * targets).sum(dim=1)
     union = preds.sum(dim=1) + targets.sum(dim=1)
     iou_denom = (preds + targets - preds * targets).sum(dim=1)
